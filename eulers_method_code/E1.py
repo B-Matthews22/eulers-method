05cff1e9af73cf4fa3021ce9f72910b909882b2a
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-11 12:27:54 +0100

    message

--- eulers_method_code/E1.py
+++ eulers_method_code/E1.py
@@ -1,27 +1,44 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 # variables
 dt      = 1
 t_max   = 10
-tau     = 2
+tau     = 3
 
 # initial conditions
-t_0, N_0 = 0, 10
+t_0, N_0 = 0, 100
 
-t = [t_0]
-N = [N_0]
+time = np.linspace(t_0,t_max,num = 100,endpoint=True)
+N = []
+t_approx = []
+N_approx = []
 
 def decay():
-    for i in range(t_0,t_max,dt):
-        N_dt = N[i] - N[i]/tau * dt
-        t_dt = t[i] + dt
+    for t in time:
+        N_dt = N_0 * np.exp(-t/tau)
         N.append(N_dt)
-        t.append(t_dt)
     return 0
 
+def approx_decay():
+    t = t_0
+    N = N_0
+
+    while t < t_max:
+        t_approx.append(t)
+        N_approx.append(N)
+        N = N - (N/tau) * dt  # Euler method update
+        t = t + dt
+
+    return t_approx, N_approx
+
+        
+
 decay()
+approx_decay()
 
 fig     =  plt.figure()
 N_vs_t  = fig.add_subplot()
-N_vs_t.plot(N,t) 
+N_vs_t.plot(time,N, color = 'red') 
+N_vs_t.plot(t_approx,N_approx, color = 'black')
 plt.show()
