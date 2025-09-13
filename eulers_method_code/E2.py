43fb2e3751674914b0a297ca28b06b5934fe4f27
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-13 15:38:21 +0100

    E2 nearly done just need to do last part

--- eulers_method_code/E2.py
+++ eulers_method_code/E2.py
@@ -3,30 +3,35 @@ import numpy as np
 
 #variables
 dt = 0.05
-t_max = 9
+t_max = 400
 
 #initial conditions
-x_0 = 1
+x_0 = [3,1,0.5,0,-0.5,-0.7]
 t_0 = 0
-position = []
-time     = []
 
 ###
 def diff_func(x_0,t_0,dt,t_max):
     x = x_0
     t = t_0
+    position = []
+    time     = []
     while t <= t_max:
         diff = t - x*x
         position.append(x), time.append(t)
         x = x + dt*diff
         t = t + dt
-    return 0
-
-diff_func(x_0,t_0,dt,t_max)
+    return position , time
 
 fig     =  plt.figure()
 x_vs_t  = fig.add_subplot()
 x_vs_t.set_xlabel("Time (s)")
 x_vs_t.set_ylabel("Position")
-x_vs_t.plot(time,position, color = 'red') 
+
+for x in x_0:
+    position, time = diff_func(x,t_0,dt,t_max)
+    x_vs_t.plot(time, position)
+
 plt.show()
+
+
+
