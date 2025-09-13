75c787fba68785153aafdb7e83eec4732508adb6
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-13 17:35:08 +0100

    should be working now i think

--- eulers_method_code/E3.py
+++ eulers_method_code/E3.py
@@ -2,8 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 #variables
 dt    = 0.02
-t_max = 20
-omega = 2
+t_max = 35
 
 #initial conditions
 x_0 = 0 
@@ -11,29 +10,36 @@ y_0 = 1
 t_0 = 0
 
 ###
+def sine_wave():
+    t = np.linspace(0,t_max,1000)
+    x = np.sin(t)
+    return x , t
+
+
 def diff_funcs(x_0,y_0,t_0,dt,t_max):
     x = x_0
     y = y_0
     t = t_0
-    positionx = []
-    positiony = []
+    position = []
     time      = []
     while t <= t_max:
+        position.append(x)
+        time.append(t)
         dx = y
-        x = x + dx * dt
         dy = -x
+        x = x + dx * dt
         y = y + dy * dt
         t = t + dt
-        positionx.append(x)
-        positiony.append(y)
-        time.append(t)
-    return positionx, positiony, time
+    return position, time
 
-positionx, positiony, time = diff_funcs(x_0,y_0,t_0,dt,t_max)
+position, time = diff_funcs(x_0,y_0,t_0,dt,t_max)
+x_exact , t_exact = sine_wave()
 
 fig     =  plt.figure()
 x_vs_t  = fig.add_subplot()
 x_vs_t.set_xlabel("Time (s)")
 x_vs_t.set_ylabel("Position")
-x_vs_t.plot(time, positionx)
+x_vs_t.plot(time, position)
+x_vs_t.plot(t_exact, x_exact)
+
 plt.show()
