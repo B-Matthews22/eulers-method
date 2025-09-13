0b2750e2d645405544b23ed73e9ebab9b7d527f4
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-13 17:55:53 +0100

    doesnt work yet

---
+++ eulers_method_code/E4.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#variables
+dt = 1
+t_max = 1
+
+#initial conditions
+t_0 = 0
+x_0 = 0
+v_0 = 1
+
+###
+
+def sine_wave():                     #to compare with euler method
+    t = np.linspace(0,t_max,1000)
+    x = np.sin(t)
+    return x , t
+
+
+def diff_funcs(x_0,v_0,t_0,dt,t_max): #eulers method to approximate sine wave
+    x , v , t = x_0 , v_0 , t_0
+    position = []
+    time     = []
+    while t <= t_max:
+        position.append(x)
+        time.append(t)
+        xinit = x + dt * v
+        vinit = v - dt * x
+        x = x + dt * (v + vinit) * 0.5
+        v = v - dt * (x + xinit) * 0.5
+    return position, time
+
+position, time = diff_funcs(x_0,v_0,t_0,dt,t_max)
+x_exact , t_exact = sine_wave()
+
+fig     =  plt.figure()
+x_vs_t  = fig.add_subplot()
+x_vs_t.set_xlabel("Time (s)")
+x_vs_t.set_ylabel("Position")
+x_vs_t.plot(time, position)
+x_vs_t.plot(t_exact, x_exact)
+
+plt.show()
