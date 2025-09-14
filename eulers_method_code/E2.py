89f80b4710fc464e21f470913aeb98380b566460
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-14 15:41:39 +0100

    x=sqrt(x) curve on graph now

--- eulers_method_code/E2.py
+++ eulers_method_code/E2.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #variables
 dt = 0.01
-t_max = 10000
+t_max = 10
 
 #initial conditions
 x_0 = [3,1,0.5,0,-0.5,-0.7]
@@ -24,13 +24,19 @@ def diff_func(x_0,t_0,dt,t_max):
 
 fig     =  plt.figure()
 x_vs_t  = fig.add_subplot()
-x_vs_t.set_xlabel("Time (s)")
-x_vs_t.set_ylabel("Position")
+
 
 for x in x_0:
     position, time = diff_func(x,t_0,dt,t_max)
     x_vs_t.plot(time, position)
 
+#This is for the x = sqrt(t) curve we need to compare our approximations to
+root_t = [np.sqrt(t) for t in time]            
+
+x_vs_t.set_xlabel("Time (s)")
+x_vs_t.set_ylabel("Position")
+x_vs_t.plot(time,root_t, color = "black", label = "x = sqrt(t)")
+x_vs_t.legend()
 plt.show()
 
 
