4b9a59b3ca9d5e0b71f826993d3bb96f361de20a
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-14 15:21:43 +0100

    Calculates area between the curves now

--- eulers_method_code/E1.py
+++ eulers_method_code/E1.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+"""
+Run the program for different size time steps, e.g. Δt={0.01, 0.005, 0.001}
+Plot both the calculated and exact values of N(t) vs. t on the same graph.
+Compare your results to the exact solution by calculating the difference between 
+the exact solution and the computed solution and see how this varies with time t.
+Comment on your results and observed errors
+"""
 # variables
-dt      = 1
+dt      = 0.5
 t_max   = 10
 tau     = 2
 
 # initial conditions
 t_0, N_0 = 0, 10
 
-time = np.linspace(t_0,t_max,num = 100,endpoint=True)
+time = np.linspace(t_0,t_max,num = int(t_max/dt)+1,endpoint=True)
 N = []
 t_approx = []
 N_approx = []
@@ -32,15 +38,19 @@ def approx_decay():
 
     return t_approx, N_approx
 
-        
 
 decay()
 approx_decay()
+
+area_difference = abs(np.trapz(N_approx,t_approx) - np.trapz(N,time))
 
 fig     =  plt.figure()
 N_vs_t  = fig.add_subplot()
 N_vs_t.set_xlabel("Time (s)")
 N_vs_t.set_ylabel("Count")
-N_vs_t.plot(time,N, color = 'red') 
-N_vs_t.plot(t_approx,N_approx, color = 'black')
+N_vs_t.plot(time, N, color = 'red', label = "True Curve") 
+N_vs_t.plot(t_approx, N_approx, color = 'black', label = "Approximated Curve")
+N_vs_t.legend()
 plt.show()
+
+print("The area difference between the true and approx functions is:" , area_difference)
