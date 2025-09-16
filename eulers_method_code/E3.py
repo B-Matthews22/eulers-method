3e4aaebb636342ff9e1f14103f72c98b17005aff
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-16 12:26:04 +0100

    need error graph

--- eulers_method_code/E3.py
+++ eulers_method_code/E3.py
@@ -11,8 +11,8 @@ y_0 = 1
 t_0 = 0
 
 ###
-def sine_wave():                     #to compare with euler method
-    t = np.linspace(0,t_max,1000)
+def sine_wave(num):                     #to compare with euler method
+    t = np.linspace(0,t_max,num)
     x = np.sin(t)
     return x , t
 
@@ -31,16 +31,17 @@ def diff_funcs(x_0,y_0,t_0,dt,t_max): #eulers method to approximate sine wave
         t = t + dt
     return position, time
 
-x_exact , t_exact = sine_wave()
 
 fig     =  plt.figure()
 x_vs_t  = fig.add_subplot()
 x_vs_t.set_xlabel("Time (s)")
 x_vs_t.set_ylabel("Amplitude (m)")
-for t in dt:
-    position, time = diff_funcs(x_0,y_0,t_0,t,t_max)
+for dt_val in dt:
+    position, time = diff_funcs(x_0,y_0,t_0,dt_val,t_max)
+    x_exact,t_exact = sine_wave(len(time))
     x_vs_t.plot(time, position,linestyle = "-")
-x_vs_t.plot(t_exact, x_exact, color = "black",linestyle = "--" , label = "x = sin(y)")
+
+x_vs_t.plot(t_exact, x_exact, color = "black",linestyle = "--" , label = "x = sin(t)")
 x_vs_t.legend(loc = 2)
 
 plt.show()
