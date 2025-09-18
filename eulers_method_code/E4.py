d87e60db42cfaeab7ca0545ed39dae811d262f1d
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-18 21:46:39 +0100

    made progress on figure

--- eulers_method_code/E4.py
+++ eulers_method_code/E4.py
@@ -5,8 +5,8 @@ import numpy as np
 need to graph the difference between approx and exact function
 '''
 #variables
-dt = 0.001
-t_max = 35
+dt = np.linspace(0.05,0.0005,5,endpoint = True)
+t_max = 10*np.pi
 
 #initial conditions
 t_0 = 0
@@ -15,35 +15,45 @@ v_0 = 1
 
 ###
 
-def sine_wave():                     #to compare with euler method
-    t = np.linspace(0,t_max,1000)
+def wave(num):                     #to compare with euler method
+    t = np.linspace(0,t_max,num)
     x = np.sin(t)
-    return x , t
-
+    v = np.cos(t)
+    return x , v , t
 
 def diff_funcs(x_0,v_0,t_0,dt,t_max): #eulers method to approximate sine wave
     x , v , t = x_0 , v_0 , t_0
     position = []
+    velocity = []
     time     = []
     while t <= t_max:
         position.append(x)
+        velocity.append(v)
         time.append(t)
         xinit = x + dt * v
         vinit = v - dt * x
         x = x + dt * (v + vinit) * 0.5
         v = v - dt * (x + xinit) * 0.5
         t = t + dt
-    return position, time
+    return position, velocity, time
 
-position, time = diff_funcs(x_0,v_0,t_0,dt,t_max)
-x_exact , t_exact = sine_wave()
 
 fig     =  plt.figure()
-x_vs_t  = fig.add_subplot()
+x_vs_t  = fig.add_subplot(1,2,1)
+err     = fig.add_subplot(1,2,2)
+
+for step in dt:
+    position, velocity, time = diff_funcs(x_0,v_0,t_0,step,t_max)
+    x_vs_t.plot(time, position)
+    #x_vs_t.plot(time, velocity)
+    x,v,t = wave(len(time))
+    err.plot(time,x - position)
+
+x_vs_t.plot(t, x,label = "x(t)",color = "black")
+#x_vs_t.plot(t, v,label = "v(t)",color = "red")
 x_vs_t.set_xlabel("Time (s)")
 x_vs_t.set_ylabel("Amplitude")
-x_vs_t.plot(t_exact, x_exact,color = "red",label = "Sine wave")
-x_vs_t.plot(time, position, color = "black",label = "Improved Euler Approximation")
+#x_vs_t.plot(t_exact, x_exact,color = "red",label = "Sine wave")
 x_vs_t.legend(loc=2)
 
 plt.show()
