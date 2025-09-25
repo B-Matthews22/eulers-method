520f81cae0abdaaf775617765f37fd2e552f61ba
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-09-25 12:47:53 +0100

    added phase_space function

--- rk4_method/R4_function.py
+++ rk4_method/R4_function.py
@@ -7,3 +7,11 @@ def damped_pendulum(t, y, b=0.1, omega0=1):
     dvdt = -b*v-(omega0**2)*x
     dydt = np.array([dxdt, dvdt])
     return dydt
+
+
+def phase_space(ax, x, v, label = None):
+    """Plot phase space (v vs. x) on a given Axes object."""
+    ax.plot(v, x, 'k',label=label)
+    ax.axis('equal')
+    ax.set_xlabel(r"$Position$")
+    ax.set_ylabel(r"$Velocity$")
