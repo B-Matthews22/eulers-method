8ae69b43178566fc35d6c67676aa58b468fbbaee
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-10-05 17:04:36 +0100

    4 plots one figure

--- graphical_methods/G3.py
+++ graphical_methods/G3.py
@@ -16,12 +16,68 @@ def main():
     stop      = 2*np.pi
     stepsize  = 0
     numpoints = 101
+    b = [0,0.5,2,5]
+    reduction = 5
+
+    x , y = gr.grid_options(start, stop, stepsize, numpoints, option = "linspace")
     
-    x , v = gr.grid_options(start, stop, stepsize, numpoints, option = "linspace")
-    dx, dv = pendulum(x, v, b = 2, omega = 1)
-    gr.reduced_density(x, v, dx, dv, label = 0, x_pos=0.9, y_pos=0.9, key_size=2, reduction=5)
+    plt.figure(figsize=(8,8))
+    plt.subplot(2,2,1)
+    plt.gca().set_aspect('equal', adjustable='box')
+    vx, vy = pendulum(x, y, b = 0, omega = 1)
+    plt.quiver(x[::reduction, ::reduction], y[::reduction, ::reduction],  # coordinates at reduced density
+                   vx[::reduction, ::reduction], vy[::reduction, ::reduction],  # arrow x/y lengths at reduced density
+                   pivot='mid',  # position of the pivot of the arrow
+                   label='SHO (b = 0)')  # label using LaTex notation
     #plt.streamplot(x, v, dx, dv)     # plot streamlines of field
-    gr.density_change(x, v, dx, dv)  # plot less streamlines of field
+    #gr.density_change(x, v, dx, dv)  # plot less streamlines of field
+    gr.both_streamlines(x, y, vx, vy)
+    plt.xlabel("x")
+    plt.ylabel("v")
+    plt.legend()
+    
+    plt.subplot(2,2,2)
+    plt.gca().set_aspect('equal', adjustable='box')
+    vx, vy = pendulum(x, y, b = 0.5, omega = 1)
+    plt.quiver(x[::reduction, ::reduction], y[::reduction, ::reduction],  # coordinates at reduced density
+                   vx[::reduction, ::reduction], vy[::reduction, ::reduction],  # arrow x/y lengths at reduced density
+                   pivot='mid',  # position of the pivot of the arrow
+                   label='b = 0.5')  # label using LaTex notation
+    #plt.streamplot(x, v, dx, dv)     # plot streamlines of field
+    #gr.density_change(x, v, dx, dv)  # plot less streamlines of field
+    gr.both_streamlines(x, y, vx, vy)
+    plt.xlabel("x")
+    plt.ylabel("v")
+    plt.legend(loc = 2)
+
+    plt.subplot(2,2,3)
+    plt.gca().set_aspect('equal', adjustable='box')
+    vx, vy = pendulum(x, y, b = 2, omega = 1)
+    plt.quiver(x[::reduction, ::reduction], y[::reduction, ::reduction],  # coordinates at reduced density
+                   vx[::reduction, ::reduction], vy[::reduction, ::reduction],  # arrow x/y lengths at reduced density
+                   pivot='mid',  # position of the pivot of the arrow
+                   label='b = 2')  # label using LaTex notation
+    #plt.streamplot(x, v, dx, dv)     # plot streamlines of field
+    #gr.density_change(x, v, dx, dv)  # plot less streamlines of field
+    gr.both_streamlines(x, y, vx, vy)
+    plt.xlabel("x")
+    plt.ylabel("v")
+    plt.legend()
+
+    plt.subplot(2,2,4)
+    plt.gca().set_aspect('equal', adjustable='box')
+    vx, vy = pendulum(x, y, b = 5, omega = 1)
+    plt.quiver(x[::reduction, ::reduction], y[::reduction, ::reduction],  # coordinates at reduced density
+                   vx[::reduction, ::reduction], vy[::reduction, ::reduction],  # arrow x/y lengths at reduced density
+                   pivot='mid',  # position of the pivot of the arrow
+                   label='b = 5')  # label using LaTex notation
+    #plt.streamplot(x, v, dx, dv)     # plot streamlines of field
+    #gr.density_change(x, v, dx, dv)  # plot less streamlines of field
+    gr.both_streamlines(x, y, vx, vy)
+    plt.xlabel("x")
+    plt.ylabel("v")
+    plt.legend(loc = 1)
+
     plt.show()
 
 
