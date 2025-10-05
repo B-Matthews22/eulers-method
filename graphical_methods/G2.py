74ed8d6831d650bf3a26e9af1df6f3cf18e1ed54
Barry Matthews <barry.matthews22@mail.dcu.ie>
2025-10-05 15:27:56 +0100

    added save function

--- graphical_methods/G2.py
+++ graphical_methods/G2.py
@@ -1,7 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import graph_function as gr
-import G1
+import pathlib as Path
+
+
+def generate_path(home_folder=str(Path.home()), subfolder='/Documents/', basename='output', extension='txt'):
+    # creates the path to store the data. Note that the data is not stored in the code repo directory.
+    # uses the method Path.home() to find the home directory in any OS
+    output_folder = home_folder + subfolder  # appends a subdirectory within it.
+    filename = basename + '.' + extension  # defines the filename the output is to be saved in
+    output_path = output_folder + filename  # creates the output path
+    return output_path
 
 
 def get_vector(x, y):
@@ -15,33 +23,41 @@ def main():
     plt.close()
 
     # Create Grid
-    coords = np.linspace(-2, 2, 101)
+    coords = np.linspace(-2*np.pi, 2*np.pi, 101)
     x, y = np.meshgrid(coords, coords)
 
     # Create Function and gradient
-    z = np.exp(-(x**2 + y**2))
+    z = np.sqrt(x**2 + y**2)
     dx, dy = np.gradient(z)  # Calculate the gradient
 
     # Create Figure
-    plt.figure(figsize=(6, 6))
-    plt.gca().set_aspect('equal', adjustable='box')  # Make plot box square
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('2D exponential function: $f(x,y)=e^{-(x^2+y^2)}$')
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.set_aspect('equal', adjustable='box')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('$f(x,y)=\sqrt{x^2+y^2}$')
 
     # Plot scalar function as color plot (contour map)
-    plt.contourf(x, y, z, 100)  # plot a contour map using N=20 levels
-    plt.set_cmap('winter_r')  # change color of map
+    contour = ax.contourf(x, y, z, 50, cmap='plasma')
+
+    # Add colorbar that matches the height of the plot
+    cbar = fig.colorbar(contour, ax=ax, fraction=0.046, pad=0.04, ticks = np.linspace(0,9,5,endpoint=True))
 
     # Plot Gradient as quiver plot
-    skip = 5  # Number of points to skip
+    skip = 5
+    x_skipped, y_skipped = x[::skip, ::skip], y[::skip, ::skip]
+    dx_skipped, dy_skipped = dx.T[::skip, ::skip], dy.T[::skip, ::skip]
 
-    # create coarse grid
-    x_skipped, y_skipped = x[::skip, ::skip], y[::skip, ::skip]  # note the indexing [start:end:skip]
-    dx_skipped, dy_skipped = dx.T[::skip, ::skip], dy.T[::skip, ::skip]  # note the .T transpose method
+    ax.quiver(x_skipped, y_skipped, dx_skipped, dy_skipped, scale=3, color='k')
 
-    # plot the gradient using the coarse grid and a quiverplot.
-    plt.quiver(x_skipped, y_skipped, dx_skipped, dy_skipped, scale=0.8)
+
+    # creates the path to store the data. Note that the data is not stored in the code repo directory.
+    filename = generate_path(basename='G2-Graph', extension='svg')  # uses the function defined above
+
+    # saves and displays the file
+    plt.savefig(filename, bbox_inches='tight')
+    print("Output file saved to {}.".format(filename))
+    
     plt.show()
 
 
